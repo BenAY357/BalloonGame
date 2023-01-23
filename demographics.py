ade8229f89e07c8c57f05deaf6a5511c31934d04
BenAY357 <96268297+BenAY357@users.noreply.github.com>
2023-01-23 18:11:27 +0000

    added doctrings to demographics methods

--- demographics.py
+++ demographics.py
@@ -19,21 +19,25 @@ class Demographics(rad_class.MultipleRadioButtonQuestions):
         window.age_error.hide() # start with age error hidden
         self.hide_gender() # hide specify gender line edit to begin with
 
-    def show_gender(self): # show specify gender line edit
+    def show_gender(self): 
+        """Show the box where people who put other can specify their gender"""
         window.specify_gender.show() 
 
     def hide_gender(self): # hide specify gender line edit
+        """Hide the box where people who put other can specify their gender"""
         window.specify_gender.hide() 
         window.specify_gender.setText("other") # reset input
 
     def get_demogs(self): # get demographics
+        """Get the responses to the demographic questions"""
         self.data["age"] = window.age.value() # get age 
         self.get_radio_responses() # get responses from radio buttons. From MultipleRadioButtonsQuestions class. 
         if gender.response == "other": # return text in specify_gender box if "other" is selected for gender. 
             gender.response = window.specify_gender.text()
             self.data["gender"] = gender.response
 
-    def on_demog_submit(self): # when next is clicked. 
+    def on_demog_submit(self): 
+        """when the demographics are submitted get the responses and check for errors"""
         self.get_demogs() # get responses
         if self.is_complete() and self.data["age"] >= 18: # move to next page if the responses are filled in and age is over 18
             pg_ctrl.next_page()
@@ -42,13 +46,15 @@ class Demographics(rad_class.MultipleRadioButtonQuestions):
             window.age_error.show() if self.data["age"] < 18 else window.age_error.hide() # show error message if age < 18. 
     ################# PID and consent submit ###################
     def on_PID_submit(self):
+        """Check that the participant have inputed their ID. If they have store it. If they haven't, show pop up warning"""
         if window.PID.text() == "": # check if PID is blank
             pop_up.pop_up_warning("Input your participant ID to continue")
         else:
             self.data["PID"] = window.PID.text() # store PID in data
             pg_ctrl.next_page() # go to next page
             
-    def on_consent_submit(self): # check if all consent boxes are ticked.
+    def on_consent_submit(self):
+        """"Check if all consent boxes are ticked"""
         for consent in window.consent_grp.children():
             if consent.isChecked() == False: # Display error message if there is an unticked box
                 return pop_up.pop_up_warning("You must consent to all of the items continue.")
